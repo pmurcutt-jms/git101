ea0b83cbc0702156e9bfc213441a46ff28cc00c3
pmurcutt <pmurcutt@johnmason.school>
2026-05-19 09:29:05 +0100

    Created dict of user data

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ name = input("What is your name?")
 age = int(input("What is your age?"))
 street_num = int(input("What is your house number?"))
 post_code = input("What is your postcode?")
+
+user_data = {"name" : name,
+             "age" : age,
+             "street_num" : street_num,
+             "post_code" : post_code }
+
+print("Hello", user_data["name"])
+
